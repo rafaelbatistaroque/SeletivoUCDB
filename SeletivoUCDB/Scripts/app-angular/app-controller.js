3a575c7d77a18e127a714ec11b3a4679939c2163
Rafael Batista <maranata_rafa@hotmail.com>
2020-02-13 10:22:40 -0400

    Adicionado método dentre view e reposítorio para obter e retorna para view a lista de tarefas existente.

--- SeletivoUCDB/Scripts/app-angular/app-controller.js
+++ SeletivoUCDB/Scripts/app-angular/app-controller.js
@@ -1,2 +1,11 @@
 ﻿appTarefas.controller('tarefaCtrl', function ($scope, tarefaServices) {
+
+    //Obter Tarefas
+    function obterListaDeTarefasParaSeremCarregadasEmTela() {
+        tarefaServices.acessarRepositorioParaObterListaTarefas().then(function (cadaTarefaRetornada) {
+            $scope.Tarefas = cadaTarefaRetornada.data;
+        }, () => alert("Erro ao obter Tarefas"));
+    };
+
+    obterListaDeTarefasParaSeremCarregadasEmTela();
 });
