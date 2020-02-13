fe67f7a1e613bc0a1a80e8a19d8c71ad76ad93cb
Rafael Batista <maranata_rafa@hotmail.com>
2020-02-13 10:44:34 -0400

    Adicionado método entre view e repositório solicitar inclusão de tarefa na lista.

--- SeletivoUCDB/Scripts/app-angular/app-controller.js
+++ SeletivoUCDB/Scripts/app-angular/app-controller.js
@@ -1,10 +1,36 @@
 ﻿appTarefas.controller('tarefaCtrl', function ($scope, tarefaServices) {
 
+
+    function limparCampos() {
+        $scope.inputTarefa = "";
+        $scope.inputHoraData = "";
+    };
+
     //Obter Tarefas
     function obterListaDeTarefasParaSeremCarregadasEmTela() {
         tarefaServices.acessarRepositorioParaObterListaTarefas().then(function (cadaTarefaRetornada) {
             $scope.Tarefas = cadaTarefaRetornada.data;
         }, () => alert("Erro ao obter Tarefas"));
+    };
+
+    //Incluir Tarefa
+    $scope.incluirNovaTarefaNaLista = function () {
+        var novoObjetoTarefa = {
+            Descricao: $scope.inputTarefa,
+            DataHora: moment($scope.inputHoraData).format("YYYY-MM-DD HH:mm:ss.SSS"),
+            StatusTarefa: "Pra fazer"
+        };
+
+        tarefaServices.acessarRepositorioParaInserirNovaTarefa(novoObjetoTarefa)
+            .then(function (respostaInclusao) {
+                if (respostaInclusao.data.success) {
+                    obterListaDeTarefasParaSeremCarregadasEmTela();
+                    alert("Adicionado!");
+                } else alert("Tarefa não adicionada");
+
+                limparCampos();
+
+            }, () => alert("Deu ruim"));
     };
 
     obterListaDeTarefasParaSeremCarregadasEmTela();
