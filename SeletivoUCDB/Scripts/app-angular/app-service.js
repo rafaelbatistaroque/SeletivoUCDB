2284e7f99c92ffe4aa76ac53a59e8f8c7423b98c
Rafael Batista <maranata_rafa@hotmail.com>
2020-02-13 10:41:44 -0400

    Adicionado serviço/repositório para inclusão de registro de tarefas.

--- SeletivoUCDB/Scripts/app-angular/app-service.js
+++ SeletivoUCDB/Scripts/app-angular/app-service.js
@@ -1,4 +1,13 @@
 ﻿appTarefas.service("tarefaServices", function ($http) {
 
     this.acessarRepositorioParaObterListaTarefas = () => $http.get("/Tarefas/ObterListaDeTarefasArmazenadasNoBD");
+
+    this.acessarRepositorioParaInserirNovaTarefa = function (novaTarefa) {
+        const requisicao = $http({
+            method: "POST",
+            url: "/Tarefas/AdicionarTarefaNoBD",
+            data: novaTarefa
+        });
+        return requisicao;
+    };
 });
