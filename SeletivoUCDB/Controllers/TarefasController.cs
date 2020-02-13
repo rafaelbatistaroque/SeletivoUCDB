82da44e2030cf9bc4886de6c1277ef9b90685a65
Rafael Batista <maranata_rafa@hotmail.com>
2020-02-13 10:18:08 -0400

    Adicionado método que realizada conexão com banco de dados e retorna lista de tarefas.

---
+++ SeletivoUCDB/Controllers/TarefasController.cs
@@ -0,0 +1,18 @@
+﻿using SeletivoUCDB.BD;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SeletivoUCDB.Controllers
+{
+    public class TarefasController : Controller
+    {
+        // GET: Tarefas/ObterListaDeTarefasArmazenadasNoBD
+        public JsonResult ObterListaDeTarefasArmazenadasNoBD()
+        {
+            using (var db = new SeletivoUCDBDbContext())
+            {
+                return Json(db.Tarefas.ToList(), JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
